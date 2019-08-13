b334c0a4960f5886be6e7173a71faa3fcfa66d35
Bill <williamkennethcarden@yahoo.com>
2019-08-13 11:15:00 -0700

    opencv7

---
+++ OpenCV/opencv7.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+cam = cv2.VideoCapture(0)
+
+while 1:
+    _, frame = cam.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([150, 180, 180])
+    upper_red = np.array([200, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    result = cv2.bitwise_and(frame, frame, mask = mask)
+
+    cv2.imshow('Frame', frame)
+    cv2.imshow('Mask', mask)
+    cv2.imshow('Result', result)
+
+    key = cv2.waitKey(5) & 0xFF
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+cam.release()
