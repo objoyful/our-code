57d8499b84cc0ec5f5351b655b6fae5899b51980
Bill <williamkennethcarden@yahoo.com>
2019-09-04 16:17:48 -0700

    update cv10

--- OpenCV/opencv10.py
+++ OpenCV/opencv10.py
@@ -1,6 +1,32 @@
 import cv2
 import numpy as np
 
-cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
+
 while(1):
+    _, frame = cam.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    src = gray
+
+    ddepth = cv2.CV_64F
+    ksize = 15
     
+    laplacian = cv2.Laplacian(src, ddepth)
+    sobelx = cv2.Sobel(src, ddepth, 1, 0, ksize = ksize)
+    sobely = cv2.Sobel(src, ddepth, 0, 1, ksize = ksize)
+
+    cv2.imshow('Frame', frame)
+    cv2.imshow('Source', src)
+    cv2.imshow('Laplacian', laplacian)
+    cv2.imshow('Sobel X', sobelx)
+    cv2.imshow('Sobel Y', sobely)
+
+    key = cv2.waitKey(10) & 0xFF
+    if key == 27 or key == ord('q'):
+        break
+    else:
+        continue
+
+cv2.destroyAllWindows()
