3b88613fdfb2bf1da22b117ffa2fe1c1a477c87a
Bill <williamkennethcarden@yahoo.com>
2019-09-06 11:34:34 -0700

    update cv10

--- OpenCV/opencv10.py
+++ OpenCV/opencv10.py
@@ -16,12 +16,14 @@ while(1):
     laplacian = cv2.Laplacian(src, ddepth)
     sobelx = cv2.Sobel(src, ddepth, 1, 0, ksize = ksize)
     sobely = cv2.Sobel(src, ddepth, 0, 1, ksize = ksize)
+    edges = cv2.Canny(frame, 100, 200)
 
     cv2.imshow('Frame', frame)
     cv2.imshow('Source', src)
     cv2.imshow('Laplacian', laplacian)
     cv2.imshow('Sobel X', sobelx)
     cv2.imshow('Sobel Y', sobely)
+    cv2.imshow('Edges', edges)
 
     key = cv2.waitKey(10) & 0xFF
     if key == 27 or key == ord('q'):
@@ -29,4 +31,5 @@ while(1):
     else:
         continue
 
+cam.release()
 cv2.destroyAllWindows()
