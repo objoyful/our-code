3a9703422a8d1fd249f6c2828009fbcfa87f6b57
Bill <47586568+awsumbill@users.noreply.github.com>
2020-08-14 14:16:43 -0700

    Edit Command.ps1
    Edited the command line crash cource.

--- LPTHW/command.ps1
+++ LPTHW/command.ps1
@@ -1,20 +1,21 @@
 # Welcome to the command line crash cource!
 # You will need to know how to use Powershell to continue Python.
 # Just like in Python, you can use '#' to comment.
-# Also, the .ps1 file extension is a Powershell Script.
 
 # Ok, in this short cource I will go through the following commands:
 
-pwd # Print working directory.
-mkdir # Make directory.
-cd # Change directory.
-ls # List directory.
-rmdir # Remove directory.
-pushd # Save your location and go to a new one.
-popd # Return to the saved location.
+pwd # [Get-Location] Print working directory.
+mkdir # [New-Item -Type D] Make directory.
+cd # [Set-Location] Change directory.
+ls # [Get-ChildItem] List directory.
+rmdir # [Remove-Item] Remove directory.
+pushd # [Push-Location] Save your location and go to a new one.
+popd # [Pop-Location] Return to the saved location.
 New-Item # Make an empty file.
-cp # Copy file from one location to another.
-mv # Move file from one location to another.
-more # View a file.
-rm # Remove a file.
-exit # Exit the terminal.
+cp # [Copy-Item] Copy file from one location to another.
+mv # [Move-Item] Move file from one location to another.
+more # Page through a file.
+cat # [Get-Content] Print the whole file.
+rm # [Remove-Item] Remove a file.
+clear # [Clear-Host] Clear the terminal.
+exit # [Exit] Exit the terminal.
