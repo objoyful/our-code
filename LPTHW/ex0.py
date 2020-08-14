362d16742ad7dc3a24d5dee44225e821b219eb38
Bill <47586568+awsumbill@users.noreply.github.com>
2020-08-14 14:30:18 -0700

    update ex0.py
    Make changes tot he ex0 python file in LPTHW.

--- LPTHW/ex0.py
+++ LPTHW/ex0.py
@@ -1,20 +1,16 @@
 # You need to know how to use the temrinal before doing this.
 # Go to command.ps1 to see the Command Line Crash Cource
 # Exercise 0 is all about seting up Python on your computer.
-# Since we all have Windows, I am going to show just the Windows instructions.
 
 # 1. Download a text editor. For our case, it is Visual Studio Code: https://code.visualstudio.com
-# 2. To get to it easier, put it on your Desktop and/or Taskbar.
-# 3. Run Powershell.
-# 4. Pn it to your taskbar by right-clicking the icon on the taskbar and selecting "Pin to taskbar".
-# 5. Open Powershell.
-# 6. Download the latest version of Python here: https://www.python.org/downloads/
+# 2. Open Powershell.
+# 3. Download the latest version of Python here: https://www.python.org/downloads/
 #    Make sure to check the box that says add Python to PATH.
-# 7. In Powershell, run Python. (python)
-# 8. Type Ctrl + Z and press enter to exit.
-# 9. You should be back to a propmt simialr to the one thatn you had when you first started Powershell.
-# 10. Make a directory to put all of your programs in. (mkdir)
-# 11. Change into that directory. (cd)
-# 12. Use VSCode to make a file.
-# 13. Go back to Powershell just using the keyboard (Alt+Tab).
-# 14. In Powershell, list the directory to see your file. (ls)
+# 4. In Powershell, run Python. (Type python in the shell.)
+# 5. Type Ctrl + Z and press enter to exit.
+# 6. You should be back to a propmt simialr to the one that you had when you first started Powershell.
+# 7. Make a directory to put all of your programs in. (mkdir)
+# 8. Change into that directory. (cd)
+# 9. Use VSCode to make a file.
+# 10. Go back to Powershell just using the keyboard (Alt+Tab).
+# 11. In Powershell, list the directory to see your file. (ls)
