cc3dfad79357368774f614e2f6ab0f65cd621ec6
Bill <47586568+awsumbill@users.noreply.github.com>
2021-01-26 12:22:32 -0700

    add prompt to powershell script

--- pypkg.ps1
+++ pypkg.ps1
@@ -1,7 +1,8 @@
 Write-Output "Download https://www.microsoft.com/en-us/download/details.aspx?id=44266 and https://visualstudio.microsoft.com/visual-cpp-build-tools/ select C++ Tools"
-py -2 -m pip install -r .\Packages\requirements-python2.txt -U --user
-py -2 -m pip install -r .\Packages\pip-python2.txt -U --user
-py -3.8 -m pip install -r .\Packages\requirements.txt -U --user
-py -3.8 -m pip install -r .\Packages\pip.txt -U --user
-py -3 -m pip install -r .\Packages\requirements.txt -U --user
-py -3 -m pip install -r .\Packages\pip-python3.9.txt -U --user
+Read-Host -Prompt "Press Enter to Continue"
+py -2 -m pip install -r .\Packages\requirements-python2.txt --upgrade --user
+py -2 -m pip install -r .\Packages\pip-python2.txt --upgrade --user
+py -3.8 -m pip install -r .\Packages\requirements.txt --upgrade --user
+py -3.8 -m pip install -r .\Packages\pip.txt --upgrade --user
+py -3 -m pip install -r .\Packages\requirements.txt --upgrade --user
+py -3 -m pip install -r .\Packages\pip-python3.9.txt --upgrade --user
