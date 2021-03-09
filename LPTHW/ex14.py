f315c659bd445931d74a73da99db71262502c8f8
Bill <47586568+awsumbill@users.noreply.github.com>
2021-03-08 23:06:03 -0700

    update try/except

--- LPTHW/ex14.py
+++ LPTHW/ex14.py
@@ -10,22 +10,22 @@ except:
     print("Please type the missing arguments below:")
     
     user_name = input("Argument Number 1: ")
-finally:
-    prompt = '> '
 
-    print(f"Hi {user_name}, I'm the {script} script.")
-    print("I'd like to ask you a few questions.")
-    print(f"Do you like me {user_name}?")
-    likes = input(prompt)
+prompt = '> '
 
-    print(f"Where do you live {user_name}?")
-    lives = input(prompt)
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
 
-    print("What kind of computer do you have?")
-    computer = input(prompt)
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
 
-    print(f"""
-    Alright, so you said {likes} about liking me.
-    You live in {lives}.  Not sure where that is.
-    And you have a {computer} computer.  Nice.
-    """)
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}.  Not sure where that is.
+And you have a {computer} computer.  Nice.
+""")
--- LPTHW/ex16.py
+++ LPTHW/ex16.py
@@ -10,33 +10,33 @@ except:
     print("Please type the missing arguments below:")
     
     filename = input("Argument Number 1: ")
-finally:
-    print(f"We're going to erase {filename}.")
-    print("If you don't want that, hit CTRL-C (^C).")
-    print("If you do want that, hit RETURN.")
 
-    input("?")
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
 
-    print("Opening the file...")
-    target = open(filename, 'w')
+input("?")
 
-    print("Truncating the file.  Goodbye!")
-    target.truncate()
+print("Opening the file...")
+target = open(filename, 'w')
 
-    print("Now I'm going to ask you for three lines.")
+print("Truncating the file.  Goodbye!")
+target.truncate()
 
-    line1 = input("line 1: ")
-    line2 = input("line 2: ")
-    line3 = input("line 3: ")
+print("Now I'm going to ask you for three lines.")
 
-    print("I'm going to write these to the file.")
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
 
-    target.write(line1)
-    target.write("\n")
-    target.write(line2)
-    target.write("\n")
-    target.write(line3)
-    target.write("\n")
+print("I'm going to write these to the file.")
 
-    print("And finally, we close it.")
-    target.close()
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
