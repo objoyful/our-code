0ba113fa2683ced6942d9f647a67ca657edc2ba9
Bill <47586568+awsumbill@users.noreply.github.com>
2021-03-08 23:38:41 -0700

    pypkgshort

---
+++ pypkgshort.sh
@@ -0,0 +1,11 @@
+#Python 2
+python -m pip install -r ./Packages/requirements-python2.txt -U --user
+python -m pip install -r ./Packages/pip-python2.txt -U --user
+
+#Python 3
+python3 -m pip install -r ./Packages/requirements.txt -U --user
+python3 -m pip install -r ./Packages/pip.txt -U --user
+
+#Python 3.9
+python3.9 -m pip install -r ./Packages/requirements.txt -U --user
+python3.9 -m pip install -r ./Packages/pip-python3.9.txt -U --user
