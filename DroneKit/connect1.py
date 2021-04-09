9d4f4bddad571b7ccf3b6fa03e55a6a3ad85760e
Bill <47586568+awsumbill@users.noreply.github.com>
2021-04-08 21:52:45 -0700

    update pip packes and dronecode

--- DroneKit/connect1.py
+++ DroneKit/connect1.py
@@ -7,19 +7,26 @@ import argparse
 
 #Function
 def connectCopter():
-    parser = argparse.ArgumentParser(description='commands')
+    parser = argparse.ArgumentParser()
     parser.add_argument('--connect')
+   
     args = parser.parse_args()
-
+    
     connection_string = args.connect
-
+    
     if not connection_string:
         import dronekit_sitl
         sitl = dronekit_sitl.start_default()
         connection_string = sitl.connection_string()
 
+        print(f"Connecting to SITL ({connection_string})")
+    else:
+        print(f"Connecting to {connection_string}")
+    
     vehicle = connect(connection_string, wait_ready=True)
+    print("Connected Succussfuly!")
 
     return vehicle
 
+#Main Executable
 vehicle = connectCopter()
