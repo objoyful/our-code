5b8269602aa4fa55e7d4c824d2f5d23cd577d4a6
Bill <47586568+awsumbill@users.noreply.github.com>
2021-06-09 17:20:02 -0700

    remove python3.8

--- pypkg.ps1
+++ pypkg.ps1
@@ -1,8 +1,6 @@
-Write-Output "Download https://www.microsoft.com/en-us/download/details.aspx?id=44266 and https://visualstudio.microsoft.com/visual-cpp-build-tools/ select C++ Tools"
+Write-Output "Download https://web.archive.org/web/20210106040224/https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi and https://visualstudio.microsoft.com/visual-cpp-build-tools/ select C++ Tools"
 Read-Host -Prompt "Press Enter to Continue"
 py -2 -m pip install -r .\Packages\requirements-python2.txt --upgrade --user
 py -2 -m pip install -r .\Packages\pip-python2.txt --upgrade --user
-py -3.8 -m pip install -r .\Packages\requirements.txt --upgrade --user
-py -3.8 -m pip install -r .\Packages\pip.txt --upgrade --user
 py -3 -m pip install -r .\Packages\requirements.txt --upgrade --user
 py -3 -m pip install -r .\Packages\pip.txt --upgrade --user
