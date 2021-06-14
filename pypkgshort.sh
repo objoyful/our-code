3f47f558c13ff14bf31960716c3584846b3cee4d
Bill <47586568+awsumbill@users.noreply.github.com>
2021-06-13 17:26:33 -0700

    Remove Python 3.8 from pypkg.sh
    All packages now work on Python 3.9 so there is no use for Python 3.8

--- pypkgshort.sh
+++ pypkgshort.sh
@@ -5,7 +5,3 @@ python -m pip install -r ./Packages/pip-python2.txt -U --user
 #Python 3
 python3 -m pip install -r ./Packages/requirements.txt -U --user
 python3 -m pip install -r ./Packages/pip.txt -U --user
-
-#Python 3.9
-python3.9 -m pip install -r ./Packages/requirements.txt -U --user
-python3.9 -m pip install -r ./Packages/pip-python3.9.txt -U --user
