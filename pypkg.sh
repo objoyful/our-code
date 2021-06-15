3e8d2957e42599ff5e2da1ca0330c29d5e7a0d37
Bill <47586568+awsumbill@users.noreply.github.com>
2021-06-15 12:53:01 -0700

    change pypkg from python3 to python3.9

--- pypkg.sh
+++ pypkg.sh
@@ -8,5 +8,5 @@ python -m pip install -r ./Packages/requirements-python2.txt -U --user
 python -m pip install -r ./Packages/pip-python2.txt -U --user
 
 #Python 3
-python3 -m pip install -r ./Packages/requirements.txt -U --user
-python3 -m pip install -r ./Packages/pip.txt -U --user
+python3.9 -m pip install -r ./Packages/requirements.txt -U --user
+python3.9 -m pip install -r ./Packages/pip.txt -U --user
