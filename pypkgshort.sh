ad431828d49067d72490bafcde4ebb474d439fc3
Bill <47586568+awsumbill@users.noreply.github.com>
2021-06-28 23:13:33 -0700

    change python to python2

--- pypkgshort.sh
+++ pypkgshort.sh
@@ -1,6 +1,6 @@
 #Python 2
-python -m pip install -r ./Packages/requirements-python2.txt -U --user
-python -m pip install -r ./Packages/pip-python2.txt -U --user
+python2 -m pip install -r ./Packages/requirements-python2.txt -U --user
+python2 -m pip install -r ./Packages/pip-python2.txt -U --user
 
 #Python 3
 python3.9 -m pip install -r ./Packages/requirements.txt -U --user
