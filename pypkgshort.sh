abfe01a673c6136dc255a3996d82a00b96dd07c1
Bill <47586568+awsumbill@users.noreply.github.com>
2021-06-30 11:54:39 -0700

    remove python 2

--- pypkgshort.sh
+++ pypkgshort.sh
@@ -1,7 +1,3 @@
-#Python 2
-python2 -m pip install -r ./Packages/requirements-python2.txt -U --user
-python2 -m pip install -r ./Packages/pip-python2.txt -U --user
-
 #Python 3
 python3.9 -m pip install -r ./Packages/requirements.txt -U --user
 python3.9 -m pip install -r ./Packages/pip.txt -U --user
