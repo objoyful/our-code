bf9f9a3188e45e4f347bbe9462a1a74e70a2a674
Bill <williamkennethcarden@yahoo.com>
2021-07-17 22:44:54 -0700

    add python-is-python3

--- pypkg.sh
+++ pypkg.sh
@@ -1,5 +1,5 @@
 #Install packages for Drone Dojo
-sudo apt install -y python3.9 python3-dev python3-wxgtk4.0 python3-yaml vim git screen libffi-dev build-essential autoconf libtool pkg-config python3-opengl python-pil pyqt5-dev-tools libqscintilla2-qt5-designer python3-qtpy libgle3 libssl-dev python3.9-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl
+sudo apt install -y python-is-python3 python3.9 python3-dev python3-wxgtk4.0 python3-yaml vim git screen libffi-dev build-essential autoconf libtool pkg-config python3-opengl python-pil pyqt5-dev-tools libqscintilla2-qt5-designer python3-qtpy libgle3 libssl-dev python3.9-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl
 python3.9 ./Packages/get-pip.py -U --user
 sudo usermod -a -G dialout $USER
 
