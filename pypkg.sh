a43a19b788a5f4aceb08759bbe065f0d4db9a418
Bill <williamkennethcarden@yahoo.com>
2021-07-21 15:54:13 -0700

    change comments on pypkg.sh

--- pypkg.sh
+++ pypkg.sh
@@ -1,8 +1,12 @@
-#Install packages for Drone Dojo
+#Install Linux packages
 sudo apt install -y python-is-python3 python3.9 python3-dev python3-wxgtk4.0 python3-yaml vim git screen libffi-dev build-essential autoconf libtool pkg-config python3-opengl python-pil pyqt5-dev-tools libqscintilla2-qt5-designer python3-qtpy libgle3 libssl-dev python3.9-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl
-python3.9 ./Packages/get-pip.py -U --user
+
+#Give user permissions to access MAVLink
 sudo usermod -a -G dialout $USER
 
-#Python 3
+#Install pip
+python3.9 ./Packages/get-pip.py -U --user
+
+#Python 3.9
 python3.9 -m pip install -r ./Packages/requirements.txt -U --user
 python3.9 -m pip install -r ./Packages/pip.txt -U --user
