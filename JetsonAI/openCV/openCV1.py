e32e52682fffb28bd24173fc728e5fd632730ee1
Bill <williamkennethcarden@yahoo.com>
2021-08-03 15:33:33 -0700

    add canberra to pypkg

--- JetsonAI/openCV/openCV1.py
+++ JetsonAI/openCV/openCV1.py
@@ -1,17 +1,29 @@
 import cv2
 
-dispW = 960
-dispH = 720
+dispW = 320 * 4
+dispH = 240 * 4
 
 flip = 4
 
 camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=' + str(flip) + ' ! video/x-raw, width=' + str(dispW) + ', height=' + str(dispH) + ', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-cam = cv2.VideoCapture(camSet)
+
+piCam = cv2.VideoCapture(camSet)
+webCam = cv2.VideoCapture(1)
+
+dualCams = False
+singleCam = webCam
 
 while True:
-    ret, frame = cam.read()
+    if dualCams:
+        piRet, piFrame = piCam.read()
+        webRet, webFrame = webCam.read()
 
-    cv2.imshow('PiCam', frame)
+        cv2.imshow('Pi Cam', piFrame)
+        cv2.imshow('Web Cam', webFrame)
+    else:
+        singleCamRet, singleCamFrame = singleCam.read()
+
+        cv2.imshow('Camera', singleCamFrame)
 
     if cv2.waitKey(1) == ord('q'):
         break
