3615ff7680e9eb7604bdfbe18b73b0db9c090ff3
Bill <williamkennethcarden@yahoo.com>
2021-08-05 13:08:01 -0700

    add opencv section to jetson ai

--- JetsonAI/openCV/openCV2.py
+++ JetsonAI/openCV/openCV2.py
@@ -10,12 +10,31 @@ camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, 
 piCam = cv2.VideoCapture(camSet)
 webCam = cv2.VideoCapture(1)
 
-cam = webCam
+cam = piCam
+cam2 = webCam
 
 while True:
     ret, frame = cam.read()
-    name = 'Camera'
+    ret, frame2 = cam2.read()
+
+    name = 'Pi Camera'
     cv2.imshow(name, frame)
+    cv2.moveWindow(name, 0, 0)
+    
+    name = 'Web Camera'
+    cv2.imshow(name, frame2)
+    cv2.moveWindow(name, dispW + 80, 0)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+    name = 'Gray Pi Camera'
+    cv2.imshow(name, gray)
+    cv2.moveWindow(name, 0, dispH + 60)
+
+    name = 'Gray Webcam'
+    cv2.imshow(name, gray2)
+    cv2.moveWindow(name, dispW + 80, dispH + 60)
 
     if cv2.waitKey(1) == ord('q'):
         break
