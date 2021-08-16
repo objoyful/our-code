53df3ff3217f946b1b00b13bb2e3f0f2933c485e
Bill <williamkennethcarden@yahoo.com>
2021-08-15 17:52:05 -0700

    update openCV 1 in JetsonAI and rename openCV5

--- JetsonAI/openCV/openCV1.py
+++ JetsonAI/openCV/openCV1.py
@@ -13,8 +13,19 @@ webCam = cv2.VideoCapture(1)
 # Web cam or pi cam setting
 cam = webCam
 
+w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+w2 = int(w / 2)
+h2 = int(h / 2)
+
 while True:
     ret, frame = cam.read()
+
+    if not ret:
+        print("Camera malfunction")
+        break
+    
     cv2.imshow('Camera', frame)
 
     if cv2.waitKey(1) == ord('q'):
