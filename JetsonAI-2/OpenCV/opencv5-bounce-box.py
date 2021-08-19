e8b168fffa8fd68685195b8e092b598aa74f9fca
Bob <robertscarden@yahoo.com>
2021-08-19 16:27:19 -0700

    it's a thing wow

--- JetsonAI-2/OpenCV/opencv5-bounce-box.py
+++ JetsonAI-2/OpenCV/opencv5-bounce-box.py
@@ -19,10 +19,14 @@ dy=2
 while True:
     ret, frame=cam.read()
     cv2.moveWindow('nanocam',0,0)
-    frame=cv2.rectangle(frame,(posX,posY),(posX+BW,posY+BH),(0,255,0),-1)
+    frame=cv2.rectangle(frame,(posX,posY),(posX+BW,posY+BH),(0,255,255),25)
     cv2.imshow('nanocam',frame)
     posX=posX+dx
     posY=posY+dy
+    if posX<=0 or posX+BW>=dispW:
+        dx=dx*(-1)
+    if posY<=0 or posY+BH>dispH:
+        dy=dy*(-1)
     if cv2.waitKey(1)==ord('q'):
         break
 cam.release()
