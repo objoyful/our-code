d59b629ca120d8abfcdd2f6702005153d2e77c56
Bill <WilliamKennethCarden@yahoo.com>
2021-09-09 12:25:35 -0700

    add openCV19 and 20 in JetsonAI

--- JetsonAI/openCV/openCV19.py
+++ JetsonAI/openCV/openCV19.py
@@ -1,4 +1,19 @@
 from adafruit_servokit import ServoKit
+import time
+import cv2
 
 myKit = ServoKit(channels = 16)
-myKit.servo[0].angle = 90
+
+while True:
+    for i in range(0, 160, 1):
+        myKit.servo[0].angle = i
+        myKit.servo[1].angle = i
+        time.sleep(0.005)
+
+    for i in range(160, 0, -1):
+        myKit.servo[0].angle = i
+        myKit.servo[1].angle = i
+        time.sleep(0.005)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
