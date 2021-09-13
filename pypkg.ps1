1cc2e47901530a46e69eeaae5504768be9ffa704
Bill <47586568+awsumbill@users.noreply.github.com>
2021-09-13 14:23:01 -0700

    remove vc for python2 link

--- pypkg.ps1
+++ pypkg.ps1
@@ -1,5 +1,5 @@
 # Begining Prompt
-Write-Output "Download https://web.archive.org/web/20210106040224/https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi and https://visualstudio.microsoft.com/visual-cpp-build-tools/ select C++ Tools"
+Write-Output "Download https://visualstudio.microsoft.com/visual-cpp-build-tools/ select C++ Tools"
 Read-Host -Prompt "Press Enter to Continue"
 
 # Python 3
