2f5fa734ee4e6e926502099cb6bf61cff11056ce
Bill <47586568+awsumbill@users.noreply.github.com>
2021-09-13 14:23:49 -0700

    change opencv-contrib-python to opencv-python

--- pypkg.ps1
+++ pypkg.ps1
@@ -5,4 +5,4 @@ Read-Host -Prompt "Press Enter to Continue"
 # Python 3
 py -3 -m pip install -r .\Packages\requirements.txt --upgrade --user
 py -3 -m pip install -r .\Packages\pip.txt --upgrade --user
-py -3 -m pip install opencv-contrib-python --upgrade --user
+py -3 -m pip install opencv-python --upgrade --user
