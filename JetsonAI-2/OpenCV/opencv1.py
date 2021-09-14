5073b5422d6e2be04c4f840228cf09c7797a108f
Bob <robertscarden@yahoo.com>
2021-09-13 17:12:47 -0700

    stuff

--- JetsonAI-2/OpenCV/opencv1.py
+++ JetsonAI-2/OpenCV/opencv1.py
@@ -1,7 +1,7 @@
 import cv2
 print(cv2.__version__)
-dispW=640
-dispH=480
+dispW=1280*1/2
+dispH=720*1/2
 flip=2
 camSet='nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 #cam=cv2.VideoCapture(camSet)
---
+++ JetsonAI-2/OpenCV/opencv19-simpleservo.py
@@ -0,0 +1,3 @@
+from adafruit_servokit import ServoKit
+myKit=ServoKit(channels=16)
+myKit.servo[0].angle=170
