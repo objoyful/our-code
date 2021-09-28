993e88a167e4dfa84d6f75acc89689619c814bcd
Bill <WilliamKennethCarden@yahoo.com>
2021-09-28 16:34:39 -0700

    add packages.txt

--- pypkg.py
+++
@@ -1,5 +0,0 @@
-from sys import exit
-
-packages = "python3-dev python3-wxgtk4.0 python3-yaml vim git screen libffi-dev build-essential autoconf libtool pkg-config python3-opengl python-pil pyqt5-dev-tools libqscintilla2-qt5-designer python3-qtpy libgle3 libssl-dev python3.8-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libfreetype6-dev python3-setuptools python3-dev python3 libportmidi-dev libhdf5-dev libxml2-dev libxslt-dev cython gfortran libatlas-base-dev libcanberra-gtk-module hdf5-tools libblas-dev libhdf5-serial-dev liblapack-dev cmake libopenblas-dev liblapack-dev libjpeg-dev"
-old_packages = "python3.8"
-new_packages = "python3.9 python-is-python3"
--- pypkgshort.sh
+++
@@ -1,3 +0,0 @@
-#Python 3
-python3.9 -m pip install -r ./Packages/requirements.txt -U --user
-python3.9 -m pip install -r ./Packages/pip.txt -U --user
