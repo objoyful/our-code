af7bfbe4f8745a9698f0b7bab8b7cb5a23f8ce7f
Bill <WilliamKennethCarden@yahoo.com>
2021-10-01 13:26:36 -0700

    add windows requirements to packages.txt and remove pypkg.ps1

--- pypkg.ps1
+++
@@ -1,8 +0,0 @@
-# Begining Prompt
-Write-Output "Download https://visualstudio.microsoft.com/visual-cpp-build-tools/ select C++ Tools"
-Read-Host -Prompt "Press Enter to Continue"
-
-# Python 3
-py -3 -m pip install -r .\Packages\requirements.txt --upgrade --user
-py -3 -m pip install -r .\Packages\pip.txt --upgrade --user
-py -3 -m pip install opencv-python --upgrade --user
