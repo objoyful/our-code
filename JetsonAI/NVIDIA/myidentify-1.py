47e322e0f41023ff2dee2e05744a2e3d47d94f68
Bill <WilliamKennethCarden@yahoo.com>
2021-10-01 12:55:32 -0700

    update nvidia foler in jetosnai

--- JetsonAI/NVIDIA/myidentify-1.py
+++ JetsonAI/NVIDIA/myidentify-1.py
@@ -1,18 +1,18 @@
 import jetson.inference
 import jetson.utils
 
-net = jetson.inference.imageNet('googleNet')
-cam = jetson.utils.gstCamera(1920, 1080, '/dev/video1')
+# net = jetson.inference.imageNet('googleNet')
+cam = jetson.utils.gstCamera(1280, 720, '/dev/video1')
 disp = jetson.utils.glDisplay()
 
-font = jetson.utils.cudaFont()
+# font = jetson.utils.cudaFont()
 
 while disp.IsOpen():
     frame, width, height = cam.CaptureRGBA()
     
-    classID, confident = net.Classify(frame, width, height)
-    item = net.GetClassDesc(classID)
+    # classID, confident = net.Classify(frame, width, height)
+    # item = net.GetClassDesc(classID)
 
-    font.OverlayText(frame, width, height, item, 5, 5, font.Magenta, font.Blue)
+    # font.OverlayText(frame, width, height, item, 5, 5, font.Magenta, font.Blue)
     
     disp.RenderOnce(frame, width, height)
