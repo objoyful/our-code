2dca5c730701788a282d8f8b124cb6b97e91bf96
Bill <47586568+awsumbill@users.noreply.github.com>
2021-11-22 13:59:10 -0700

    add ml 9-19 in Machine Learning

--- MachineLearning/ml6.py
+++ MachineLearning/ml6.py
@@ -14,13 +14,12 @@ df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume', ]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
-
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forcast_col = 'Adj. Close'
 df.fillna(-999999, inplace=True)
 
-forcast_out = int(math.ceil(0.01 * len(df)))
+forcast_out = int(math.ceil(0.1 * len(df)))
 print(forcast_out)
 
 df['label'] = df[forcast_col].shift(-forcast_out)
@@ -28,8 +27,8 @@ df['label'] = df[forcast_col].shift(-forcast_out)
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
-X = X[:-forcast_out]
 X_lately = X[-forcast_out:]
+X = X[:-forcast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
