19411e03ebdcb223fd8d4aca38a4720c22af7ebe
Bill <47586568+awsumbill@users.noreply.github.com>
2022-11-11 15:00:58 -0700

    update ml course

--- MachineLearning/ml2.py
+++ MachineLearning/ml2.py
@@ -2,7 +2,9 @@ import quandl
 import pandas as pd
 import math
 
+
 df = quandl.get('WIKI/GOOGL')
+print(type(df))
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume', ]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
--- MachineLearning/ml5.py
+++ MachineLearning/ml5.py
@@ -1,3 +1,4 @@
+# Imports
 import quandl, math, datetime
 import pandas as pd
 import numpy as np
@@ -9,29 +10,33 @@ from matplotlib import style
 
 style.use('ggplot')
 
+# Data Setup
 df = quandl.get('WIKI/GOOGL')
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume', ]]
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+# Creating Features
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
+# X
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
+# Y
 forcast_col = 'Adj. Close'
 df.fillna(-999999, inplace=True)
 
-forcast_out = int(math.ceil(0.02 * len(df)))
+
+forcast_out = int(math.ceil(0.01 * len(df)))
 print(forcast_out)
 
 df['label'] = df[forcast_col].shift(-forcast_out)
 
-
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
-X = X[:-forcast_out]
 X_lately = X[-forcast_out:]
+X = X[:-forcast_out]
 
 df.dropna(inplace=True)
-y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.1)
@@ -39,8 +44,6 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_
 clf = LinearRegression()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
-
-# print(accuracy)
 
 forcast_set = clf.predict(X_lately)
 print(forcast_set, accuracy, forcast_out)
@@ -63,5 +66,5 @@ df['Forcast'].plot()
 
 plt.legend(loc=4)
 plt.xlabel('Date')
-plt.ylabel('Time')
+plt.ylabel('Price')
 plt.show()
