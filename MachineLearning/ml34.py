647a909711443bcbe13ca221af7156cd4bcc2226
Bill <47586568+awsumbill@users.noreply.github.com>
2022-12-09 15:32:25 -0700

    update ml 34

--- MachineLearning/ml34.py
+++ MachineLearning/ml34.py
@@ -1,11 +1,13 @@
+# Matplotlib Imports
 import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
+# Imports
 import numpy as np
 from sklearn.cluster import KMeans
-from ml38 import K_Means as Our_KMeans
 
+# Sample Data
 X = np.array([[1, 2],
               [1.5, 1.8],
               [5, 8],
@@ -13,18 +15,24 @@ X = np.array([[1, 2],
               [1, 0.6],
               [9, 11]])
 
-# plt.scatter(X[:,0], X[:,1], s = 150)
+# Plot Data
+# plt.scatter(X[:,0], X[:,1], s=150)
 # plt.show()
 
-clf = KMeans(n_clusters = 2)
+# KMeans ALgorithim
+clf = KMeans(n_clusters=2)
 clf.fit(X)
 
+# Define centroids and labels
 centroids = clf.cluster_centers_
 labels = clf.labels_
 
-colors = ["g.", "r.", "c.", "b.", "k.", "o."]
+# Define colors
+colors = 10 * ["g.", "r.", "c.", "b.", "k."]
 
+# Plot
 for i in range(len(X)):
-    plt.plot(X[i][0], X[i][1], colors[labels[1]], markersize = 10)
-plt.scatter(centroids[:, 0], centroids[:, 1], marker = 'x', s = 150, linewidths = 5)
+    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=20)
+
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=150, linewidths=5)
 plt.show()
