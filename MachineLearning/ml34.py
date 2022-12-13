241d02451c979869b6b9acda9dbdce2862873b96
Bill <47586568+awsumbill@users.noreply.github.com>
2022-12-13 15:28:49 -0700

    update ml32-35

--- MachineLearning/ml34.py
+++ MachineLearning/ml34.py
@@ -19,13 +19,13 @@ X = np.array([[1, 2],
 # plt.scatter(X[:,0], X[:,1], s=150)
 # plt.show()
 
-# KMeans ALgorithim
-clf = KMeans(n_clusters=2)
+# KMeans Algorithim
+clf = KMeans(n_clusters=3)
 clf.fit(X)
 
 # Define centroids and labels
-centroids = clf.cluster_centers_
-labels = clf.labels_
+centroids = clf.cluster_centers_ # [[x1, y1], [x2, y2]]
+labels = clf.labels_ # [0, 0, 1, 1, 0, 1]
 
 # Define colors
 colors = 10 * ["g.", "r.", "c.", "b.", "k."]
@@ -34,5 +34,6 @@ colors = 10 * ["g.", "r.", "c.", "b.", "k."]
 for i in range(len(X)):
     plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=20)
 
+#                    [x1, x2]         [y1, y2]
 plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=150, linewidths=5)
 plt.show()
