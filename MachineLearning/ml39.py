bcf5745cdd28ce8fdc3cfa4757dd4464e8277dcf
Bill <47586568+awsumbill@users.noreply.github.com>
2022-12-16 15:17:12 -0700

    update ml39

--- MachineLearning/ml39.py
+++ MachineLearning/ml39.py
@@ -6,11 +6,11 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import style
 style.use("ggplot")
 
-centers = [[1,1,1],[5,5,5],[3,10,10]]
+centers = [[1, 1, 1], [5, 5, 5], [3, 10, 10]]
 
-X, _ = make_blobs(n_samples = 1000, centers = centers, cluster_std = 1.5)
+X, _ = make_blobs(n_samples=100000, centers=centers, cluster_std=1)
 
-ms = MeanShift()
+ms = MeanShift(n_jobs=-1)
 ms.fit(X)
 labels = ms.labels_
 cluster_centers = ms.cluster_centers_
@@ -19,14 +19,12 @@ print(cluster_centers)
 n_clusters_ = len(np.unique(labels))
 print("Number of estimated clusters:", n_clusters_)
 
-colors = 10*['r','g','b','c','k','y','m']
+colors = 10 * ['r', 'g', 'b', 'c', 'k', 'y', 'm']
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 for i in range(len(X)):
     ax.scatter(X[i][0], X[i][1], X[i][2], c=colors[labels[i]], marker='o')
 
-ax.scatter(cluster_centers[:,0],cluster_centers[:,1],cluster_centers[:,2],
-            marker="x",color='k', s=150, linewidths = 5, zorder=10)
-
+ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1], cluster_centers[:, 2], marker="x", color='k', s=150, linewidths=5, zorder=10)
 plt.show()
