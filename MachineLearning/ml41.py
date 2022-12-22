d67ac6269439fdb6d614f2c28dad17b506b72d9d
Bill <47586568+awsumbill@users.noreply.github.com>
2022-12-22 15:05:54 -0700

    update ml41

--- MachineLearning/ml41.py
+++ MachineLearning/ml41.py
@@ -1,10 +1,13 @@
+# Matplotlib Setup
 import matplotlib.pyplot as plt
 from matplotlib import style
 from numpy.lib.arraysetops import unique
 style.use('ggplot')
 
+# Imports
 import numpy as np
 
+# Sample Data
 X = np.array([[1, 2],
               [1.5, 1.8],
               [5, 8],
@@ -15,73 +18,91 @@ X = np.array([[1, 2],
               [10, 2],
               [9, 3],])
 
+# Plot Sample Data
 # plt.scatter(X[:, 0], X[:, 1], s = 150)
 # plt.show()
 
+# Colors List
 colors = 10 * ["g", "r", "c", "b", "k"]
 
+# Mean Shift algorithm
 class MeanShift:
-    def __init__(self, radius = 4):
+    # Initialization
+    def __init__(self, radius=4):
         self.radius = radius
 
     def fit(self, data):
+        # Our centroids, at first every point is a centroid
         centroids = {}
 
+        # Set every point as a centroid
         for i in range(len(data)):
             centroids[i] = data[i]
         
         while True:
+            # Our new centroids will be saved here
             new_centroids = []
             
             for i in centroids:
+                # All the points that are within our bandwidth
                 in_bandwidth = []
                 centroid = centroids[i]
 
                 for featureset in data:
-                    print(f"Normalized: {np.linalg.norm(featureset - centroid)}")
-                    print(f"Feature Set: {featureset}")
-                    print(f"Centroid: {centroid}")
-                    print("============")
-                    
+                    # If that point is within our bandwidth
                     if np.linalg.norm(featureset - centroid) < self.radius:
+                        # Add point to in_bandwidth list
                         in_bandwidth.append(featureset)
                 
-                new_centroid = np.average(in_bandwidth, axis = 0)
+                # Move centroid to the middle of the bandwidth
+                new_centroid = np.average(in_bandwidth, axis=0)
                 new_centroids.append(tuple(new_centroid))
-
+            
+            # Get unique centroids
             uniques = sorted(list(set(new_centroids)))
             
+            # Save previous centroids and cler centroids variable
             prev_centroids = dict(centroids)
             centroids = {}
 
+            # Add our new centroids to centroids dictionary
             for i in range(len(uniques)):
                 centroids[i] = np.array(uniques[i])
             
             optimized = True
 
+            # Check if new centroids are diffeent from old centroids
             for i in centroids:
+                # If the new centroids and old centroids are not equal, we aren't optimized
                 if not np.array_equal(centroids[i], prev_centroids[i]):
                     optimized = False
                 
+                # If one centroid isn't optimized, we don't have to check the others
                 if not optimized:
                     break
             
+            # One we are finnaly optimized, break out of while true loop
             if optimized:
                 break
         
+        # Save centroids to self
         self.centroids = centroids
     
     def predict(self, data):
         pass
 
+# Fit mean shift
 clf = MeanShift()
 clf.fit(X)
 
+# Define centroids
 centroids = clf.centroids
 
-plt.scatter(X[:, 0], X[:, 1], s = 150)
+# Plot points
+plt.scatter(X[:, 0], X[:, 1], s=150)
 
+# Plot centroids
 for c in centroids:
-    plt.scatter(centroids[c][0], centroids[c][1], color = 'k', s = 150, marker = '*')
+    plt.scatter(centroids[c][0], centroids[c][1], color='k', s=150, marker='*')
 
 plt.show()
