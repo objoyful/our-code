3bffe9a825088bf9536464d531ef9c6a8dc6ccf3
Bill <WilliamKennethCarden@yahoo.com>
2023-01-19 15:35:49 -0700

    update ml45

--- MachineLearning/ml45.py
+++ MachineLearning/ml45.py
@@ -1,1 +1,18 @@
-import tensorflow._api.v2.compat.v1 as tf
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+result = tf.multiply(x1, x2)
+
+print(result)
+
+# sess = tf.Session()
+# print(sess.run(result))
+# sess.close()
+
+with tf.Session() as sess:
+    output = sess.run(result)
+
+print(output)
