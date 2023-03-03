b709dfc25751144b71d35a99e880095cb5f34a78
Bill <WilliamKennethCarden@yahoo.com>
2023-03-03 14:01:25 -0700

    update ml58

--- MachineLearning/ml58.py
+++ MachineLearning/ml58.py
@@ -1,15 +1,23 @@
 import tensorflow._api.v2.compat.v1 as tf
-import tensorflow.keras.datasets.mnist as mn
+import pandas as pd
+import numpy as np
 # from tensorflow.examples.tutorials.mnist import input_data
+tf.disable_eager_execution()
 
-# minst = input_data.read_data_sets('\\temp\data\\', one_hot = True)
-mnist = mn.load_data()
+# minst = input_data.read_data_sets('/temp/data/', one_hot=True)
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data() #(x_train, y_train), (x_test, y_test)
+
+x_train = x_train.flatten().reshape(len(x_train), 784) 
+x_test = x_test.flatten().reshape(len(x_test), 784)
+
+y_train = pd.get_dummies(y_train).to_numpy()
+y_test = pd.get_dummies(y_test).to_numpy()
 
 n_classes = 10
 batch_size = 128
+hm_epochs = 10
 
 # height x width
-tf.disable_eager_execution()
 x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
 
@@ -17,11 +25,11 @@ keep_rate = 0.8
 keep_prob = tf.placeholder(tf.float32)
 
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides = [1, 1, 1, 1], padding = 'SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def maxpool2d(x):
-    #                                size of window          movement of window
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+    #                        size of window      movement of window
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding = 'SAME')
 
 def convolutional_neural_network(x):
     weights = {'W_conv1':tf.Variable(tf.random_normal([5, 5, 1, 32])),
@@ -34,7 +42,7 @@ def convolutional_neural_network(x):
               'b_fc':tf.Variable(tf.random_normal([1024])),
               'out':tf.Variable(tf.random_normal([n_classes]))}
     
-    x = tf.reshape(x, shape = [-1, 28, 28, 1])
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
     
     conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
     conv1 = maxpool2d(conv1)
@@ -44,7 +52,8 @@ def convolutional_neural_network(x):
 
     fc = tf.reshape(conv2, [-1, 7 * 7 * 64])
     fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
-    fc = tf.nn.dropout(fc, keep_rate)
+
+    fc = tf.nn.dropout(fc, keep_prob=keep_rate)
 
     output = tf.matmul(fc, weights['out']) + biases['out']
 
@@ -52,26 +61,33 @@ def convolutional_neural_network(x):
 
 def train_neural_network(x):
     prediction = convolutional_neural_network(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = prediction, labels = y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
     
-    hm_epochs = 10
-    
     with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
         
         for epoch in range(hm_epochs):
             epoch_loss = 0
             
-            for _ in range(int(mnist.train.num_examples / batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
+            i = 0
+            while i < len(x_train):
+                start = i
+                end = i + batch_size
+
+                batch_x = np.array(x_train[start:end])
+                batch_y = np.array(y_train[start:end])
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                epoch_loss += c
+
+                i += batch_size
             
-            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+            print('Epoch', epoch + 1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
 
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+        print('Accuracy:', accuracy.eval({x: x_test, y: y_test}))
 
 train_neural_network(x)
