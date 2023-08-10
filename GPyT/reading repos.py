0dc91be5e401e47b5c97c473f13c1b2cca3043a7
Bill <williamkennethcarden@yahoo.com>
2023-08-10 14:44:14 -0700

    update gitignore and reading repos

--- GPyT/reading repos.py
+++ GPyT/reading repos.py
@@ -2,13 +2,15 @@ from github import Github
 import os
 import time
 from datetime import datetime
-from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, blue, cyan
+# from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, blue, cyan
 
-end_time = time.time()
+end_time = 1691379494.951034
 start_time = end_time-86400
 
-ACCESS_TOKEN = open(os.path.join("GPyT", "token.txt"), 'r').read()
-g = Github(ACCESS_TOKEN)
+BILL_TOKEN = open(os.path.join("GPyT", "token.txt"), 'r').read()
+BOB_TOKEN = open(os.path.join("GPyT", "bob-token.txt"), 'r').read()
+
+g = Github(BOB_TOKEN)
 print(g.get_user())
 
 for i in range(50):
@@ -26,10 +28,10 @@ for i in range(50):
             print(f"{repository.clone_url}")
             os.system(f"git clone {repository.clone_url} {os.path.join('GPyT', 'repos', repository.owner.login, repository.name)}")
 
-            print(cyan(bold(f"current system time {start_time}")))
+            print(f"current system time {start_time}")
     except Exception as e:
         print(str(e))
-        print(red(bold("Broke for some reason")))
+        print("Broke for some reason")
         
         end_time -= 86400
         start_time -= 86400
