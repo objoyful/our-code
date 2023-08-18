45a2b28cf7e6f7b526a631d218591080a4e17732
Bill <WilliamKennethCarden@yahoo.com>
2023-08-17 20:08:14 -0700

    add tokenize file

---
+++ GPyT/tokenize_file.py
@@ -0,0 +1,42 @@
+from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, blue, cyan
+from tokenizers import ByteLevelBPETokenizer
+from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
+import os
+
+TRAIN_BASE = True
+paths = [os.path.join("GPyT", "python_code.txt")]
+tokenizer = ByteLevelBPETokenizer()
+
+if TRAIN_BASE:
+
+    tokenizer.train(files=paths, vocab_size=52000, min_frequency=2, special_tokens=[
+        '<s>',
+        '<pad>',
+        '</s>',
+        '<unk>',
+        '<mask>'
+    ])
+
+    try:
+        os.makedirs(os.path.join("GPyT", "tokenizer"))
+    except:
+        pass
+    tokenizer.save_model(os.path.join("GPyT", "tokenizer"))
+
+inp = "print('Hello World')"
+# t = tokenizer.encode(inp)
+# print(t.ids)
+# print(t.tokens)
+
+tokenizer = GPT2Tokenizer.from_pretrained(os.path.join("GPyT", "tokenizer"))
+tokenizer.add_special_tokens({
+    "eos_token": '</s>',
+    "bos_token": '<s>',
+    "unk_token": '<unk>',
+    "pad_token": '<pad>',
+    "mask_token": '<mask>'
+})
+
+t = tokenizer.encode(inp)
+print(t)
+print(tokenizer.decode(t))
