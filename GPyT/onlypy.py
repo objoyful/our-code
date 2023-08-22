4e726525b6590e9f998450d759c7e2196b533102
Bill <WilliamKennethCarden@yahoo.com>
2023-08-21 17:47:24 -0700

    add test model

--- GPyT/onlypy.py
+++ GPyT/onlypy.py
@@ -4,6 +4,7 @@ import time
 
 root_dir = os.path.abspath(os.sep)
 d = os.path.join(root_dir, "mnt", "e", "repos")
+days = os.listdir(d)
 
 for dirpath, dirnames, filenames in os.walk(d):
     for f in filenames:
@@ -18,15 +19,18 @@ for dirpath, dirnames, filenames in os.walk(d):
             if d in full_path:
                 os.remove(full_path)
             else:
-                print(yellow("Somethings wrong"))
+                print(yellow("Somethinonlypy.pygs wrong"))
                 time.sleep(60)
-while True:
-    empty_dirs = 0
-    for dirpath, dirnames, filenames in os.walk(d):  
-        if len(filenames) == 0 and len(dirnames) == 0:
-            empty_dirs += 1
-            print(red(f"Removing {dirpath}"))
-            os.rmdir(dirpath)
-    
-    if empty_dirs == 0:
-        break
+for day in days:
+    d = os.path.join(d, day)
+    while True:
+        empty_dirs = 0
+        for dirpath, dirnames, filenames in os.walk(d):  
+            if len(filenames) == 0 and len(dirnames) == 0:
+                empty_dirs += 1
+                print(red(f"Removing {dirpath}"))
+                os.rmdir(dirpath)
+        
+        if empty_dirs == 0:
+            break
+    print(green(f"Checked {day}"))
